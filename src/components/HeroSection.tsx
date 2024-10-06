0003829272a990fc8ee5e9c56c21631363e74935
Emon Ahmed <imonshomon@gmail.com>
2024-10-06 15:42:03 +0600

    hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,21 +1,25 @@
 const HeroSection = () => {
   return (
-    <section className=" pt-[60px] lg:pt-[140px] lg:pb-[90px]">
-      <div className="max-w-[1170px] mx-auto px-5 xl:px-0 font-Poppis">
-        <div className="max-w-[770px] mx-auto text-center">
-          <h1 className="text-[30px] leading-[40px] md:text-[70px] md:leading-[80px] font-bold text-black mb-3 md:mb-5">
-            Find and Book the Best Sports{" "}
-            <span className="text-[#3d85ff]">Facilities</span>
-          </h1>
-          <p className=" text-lg md:text-xl text-black mb-3 md:mb-5">
-            Your gateway to top-tier sports venues—anytime, anywhere.
-          </p>
-          <a href="#nav">
-            <button className="bg-[#3d85ff] font-semibold text-white  px-6 py-2 rounded-full">
-              Book Now
-            </button>
-          </a>
-          <p className="mt-4 text-white">Rated 4.8/5 by thousands of players</p>
+    <section className="h-screen   2xl:h-[650px] bg-[url('https://img.freepik.com/premium-photo/dynamic-sports-background-vector-sports-day-perfect-gift-certificates-banners-flyers_444642-27222.jpg?w=826')] bg-cover bg-center bg-no-repeat bg-red-500">
+      <div className=" bg-[#f9f9f966]  w-full h-full  flex justify-center items-center">
+        <div className="max-w-[1170px] mx-auto px-5 xl:px-0 font-Poppis">
+          <div className="max-w-[770px] mx-auto text-center">
+            <h1 className="text-[30px] leading-[40px] md:text-[70px] md:leading-[80px] font-bold text-black mb-3 md:mb-5">
+              Find and Book the Best Sports{" "}
+              <span className="text-[#3d85ff]">Facilities</span>
+            </h1>
+            <p className=" text-lg md:text-xl text-black mb-3 md:mb-5">
+              Your gateway to top-tier sports venues—anytime, anywhere.
+            </p>
+            <a href="#nav">
+              <button className="bg-[#3d85ff] font-semibold text-white  px-6 py-2 rounded-full">
+                Book Now
+              </button>
+            </a>
+            <p className="mt-4 tex-black">
+              Rated 4.8/5 by thousands of players
+            </p>
+          </div>
         </div>
       </div>
     </section>
